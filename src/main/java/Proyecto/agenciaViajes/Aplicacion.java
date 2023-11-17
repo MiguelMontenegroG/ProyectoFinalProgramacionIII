fdb02ebd2d1128b5e2b6e89f703410a7219d6b99
kevin Buitron <106721566+kevinBuitron@users.noreply.github.com>
2023-11-17 18:00:27 -0500

    dvKevin
    commit  de  cliente , actualización perfil ,  paquetes en tabla , diltro de paquetes segun precio,  nombre ,

--- src/main/java/Proyecto/agenciaViajes/Aplicacion.java
+++ src/main/java/Proyecto/agenciaViajes/Aplicacion.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Aplicacion extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader( Aplicacion.class.getResource("/views/Login.fxml") );
+        FXMLLoader loader = new FXMLLoader( Aplicacion.class.getResource("/views/clientePrincipal.fxml") );
         Parent parent = loader.load();
         Scene scene = new Scene(parent);
         stage.setScene(scene);
